0e669dc971dbe96f8a65c32d993f845e94aec498
Leobelle Joseph <leobellejoseph@outlook.com>
2023-10-15 11:16:19 +0800

    Fix: experimental sliver footer

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -36,10 +35,20 @@ class _HomeScreenState extends State<HomeScreen> {
               delegate: SliverHeaderDelegate(title: 'Section 1', backgroundColor: _backgroundColor),
             ),
             SliverPadding(
-              padding: const EdgeInsets.only(bottom: 8.0, left: 6, right: 6),
+              padding: const EdgeInsets.only(left: 6, right: 6),
               sliver: _getDecoratedSliverList(10),
             ),
+            const SliverToBoxAdapter(
+                child: Divider(
+              height: 1,
+              thickness: 0.5,
+              color: Colors.white,
+              indent: 8,
+              endIndent: 8,
+            )),
+            const SliverPinnedPersistentFooter(delegate: SliverFooterDelegate(title: '')),
           ]),
+          const SliverToBoxAdapter(child: SizedBox(height: 10)),
           SliverMainAxisGroup(slivers: [
             SliverPinnedPersistentHeader(
               delegate: SliverHeaderDelegate(title: 'Section 2', backgroundColor: _backgroundColor),
@@ -139,7 +148,6 @@ class _HomeScreenState extends State<HomeScreen> {
     return DecoratedSliver(
       decoration: BoxDecoration(
         color: Colors.grey.shade200.withOpacity(0.2),
-        borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
       ),
       sliver: SliverList.separated(
         itemBuilder: (_, int index) => Padding(
@@ -188,6 +196,61 @@ class SliverHeaderDelegate extends SliverPersistentHeaderDelegate {
           child: Container(
             decoration: BoxDecoration(
               borderRadius: const BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+              color: Colors.grey.shade200.withOpacity(0.2),
+            ),
+            width: double.infinity,
+            height: headerHeight,
+            child: Center(
+              child: Text(title),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  double get maxExtent => minExtent;
+
+  @override
+  double get minExtent => 50;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+  }
+}
+
+class SliverPinnedPersistentFooter extends SliverPersistentHeader {
+  const SliverPinnedPersistentFooter({super.key, required super.delegate});
+  @override
+  bool get pinned => true;
+}
+
+class SliverFooterDelegate extends SliverPersistentHeaderDelegate {
+  final String title;
+  final double headerHeight;
+  final Color backgroundColor;
+  const SliverFooterDelegate(
+      {required this.title, this.headerHeight = 50, this.backgroundColor = Colors.lightBlueAccent});
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: backgroundColor,
+          ),
+          child: Center(
+            child: Text(title),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 6, right: 6),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
               color: Colors.grey.shade200.withOpacity(0.2),
             ),
             width: double.infinity,
