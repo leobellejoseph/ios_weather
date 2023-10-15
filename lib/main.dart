55d148689c5ad4d3a67c31955b8d5ffd0d79ee72
Leobelle Joseph <leobellejoseph@outlook.com>
2023-10-15 15:50:09 +0800

    Fix: refactor const declaration

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
