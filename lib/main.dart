8ef5fa4f67194906de16785cdb7c75428ad5dcbc
Leobelle Joseph <leobellejoseph@outlook.com>
2023-10-15 15:47:22 +0800

    Fix: portrait up orientation constraint

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ios_weather/screen/home_screen.dart';
 
 void main(List<String> args) {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MainApp());
 }
 
